fbd1ac108c516143c66416ad9fef92f3753a3e1d
Sean San <seanksan@gmail.com>
2026-02-27 21:28:50 -0800

    feat: Add HomePage and NavBar components

--- frontend/my-first-react-app/src/main.jsx
+++ frontend/my-first-react-app/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Routes, Route } from 'react-router-dom'
+// import { useState, useEffect } from 'react'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import NavBar from './components/NavBar';
+
+import HomePage from './pages/HomePage';
+
+import './styles/styles.css'
+import './styles/home-page.css'
+
+
+// Defines what Page Component appears for each of the webpages 
+// based on the current path
+function App() {
+    return (
+    <div>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    </div>
+    )
+}
+
+export default App

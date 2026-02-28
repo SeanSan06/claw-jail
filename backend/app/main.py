077b7b639be3769c9db2ff7db9d0d3606eb61eaa
Lien Jabujab <lbjabujab58@gmail.com>
2026-02-28 08:40:18 -0800

    Integrate CORS with main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <-- ADD THIS
 from app.api import router as api_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_name)
 
-app.include_router(api_router, prefix="/api")
+# ALLOW REACT TO TALK TO FASTAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In development, this is fine
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 async def root():

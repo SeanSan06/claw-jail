7e48fe1a25f0cee36bd2b7b93a51fb1df496bfa2
Sean San <seanksan@gmail.com>
2026-02-28 02:17:26 -0800

    feat: Add basic content for SecurityRuleSelector component

--- frontend/src/components/SecurityRuleSelector.jsx
+++ frontend/src/components/SecurityRuleSelector.jsx
@@ -2,12 +2,27 @@
 // For example, we can have a safe, flexible, and aggressive mode. 
 // Each mode will have different settings for the bot, and will change how the bot 
 // behaves in certain situations.
-function SecurityRuleSelector() {
+function SecurityModeSelector() {
     return  (
-        <div>
-            <Form />
+        <div id="security-mode-selector-area">
+            <h2>Security Mode Selected</h2>
+            <div id="security-mode-safe">
+                <h3>Mode: Safe</h3>
+            </div>
+            <div id="security-mode-flexible">
+                <h3>Mode: Flexible</h3>
+            </div>
+            <div id="security-mode-aggressive">
+                <h3>Mode: Aggressive</h3>
+            </div>
+            <div id="security-mode-focused">
+                <h3>Mode: Focused</h3>
+            </div>
+            <div id="security-mode-custom">
+                <h3>Mode: Custom</h3>
+            </div>
         </div>
     );
 }
 
-export default SecurityRuleSelector;
+export default SecurityModeSelector;

68e3a702a66e03a81c1b4218e8c94edeaf54a7db
Lien Jabujab <lbjabujab58@gmail.com>
2026-02-28 12:05:11 -0800

    Remove unnecessary code
    -Removed code and components in homepage.jsx and home-page.css

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,13 +5,8 @@ import SecurityModeSelected from '../components/SecurityModeSelected';
 function HomePage() {
     return (
         <div id="home-page">
-            {/* Column 1 */}
             <LiveActivityLog />
-            
-            {/* Column 2 (Now contains WhisperFlow inside it) */}
             <WhisperFlowChat />
-            
-            {/* Column 3 */}
             <SecurityModeSelected />
         </div>
     );
